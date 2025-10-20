8657e492e8ca3373a872a2116c94b581c12e2dfe
Daniel BT <danibt757@gmail.com>
2025-10-20 21:18:45 +0200

    Update Tema2ejercicio30.java

--- src/tema2/Tema2ejercicio30.java
+++ src/tema2/Tema2ejercicio30.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Tema2ejercicio30 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("*****CALCULADORA*****");
+        System.out.println("CALCULADORA");
         System.out.println("introduce un numer");
         int num1 = in.nextInt();
         System.out.println("introduce otro numer");
