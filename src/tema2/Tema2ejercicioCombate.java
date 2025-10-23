738527017dbe2cefcef40af65bc33ee7369933d3
danbartor <danibt757@gmail.com>
2025-10-23 12:21:04 +0200

    .

--- src/tema2/Tema2ejercicioCombate.java
+++ src/tema2/Tema2ejercicioCombate.java
@@ -12,8 +12,7 @@ public class Tema2ejercicioCombate {
 
         System.out.println("*******Bienvenido*****");
 
-        //Datos de jugador 1 y bucle para que no pase de 500 puntos
-
+        //Datos de jugador 1 y bucle para que no pase de 500 puntos y no tenga atributos negativos
         System.out.println("Eres el jugador 1");
         System.out.println("Antes de introducir  los datos recuerda que el maximo de puntos en total son 500 puntos, si en un dato te pasas de 200 puntos se asignará auntomaticamente 200 puntos");
         System.out.println("Introduce la vida que tiene tu personaje");
@@ -29,8 +28,8 @@ public class Tema2ejercicioCombate {
         int ataque_personaje = in.nextInt();
         if (ataque_personaje > 200) ataque_personaje = 200;
         int media1 = defensa_personaje + velocidad_personaje + ataque_personaje + vida_personaje;
-        while (media1 > 500) {
-            System.out.println("El personaje pasa de 500 puntos maximos que se reparten en las estadisticas");
+        while (media1 > 500 || vida_personaje <= 0 || defensa_personaje <= 0 || velocidad_personaje <= 0 || ataque_personaje <= 0) {
+            System.out.println("El personaje pasa de 500 puntos maximos que se reparten en las estadisticas, o has introducido atributos 0 o menores");
             System.out.println("Vuelve a introducir los datos");
             System.out.println("Vida del personaje");
             vida_personaje = in.nextInt();
@@ -63,7 +62,7 @@ public class Tema2ejercicioCombate {
         int ataque_personaje2 = in.nextInt();
         if (ataque_personaje2 > 200) ataque_personaje2 = 200;
         int media2 = defensa_personaje2 + velocidad_personaje2 + ataque_personaje2 + vida_personaje2;
-        while (media2 > 500) {
+        while (media2 > 500 || vida_personaje2 <= 0 || defensa_personaje2 <= 0 || velocidad_personaje2 <= 0 || ataque_personaje2 <= 0) {
             System.out.println("El personaje pasa de 500 puntos maximos que se reparten en las estadisticas");
             System.out.println("Vuelve a introducir los datos");
             System.out.println("Vida del personaje");
@@ -80,61 +79,102 @@ public class Tema2ejercicioCombate {
             if (ataque_personaje2 > 200) ataque_personaje2 = 200;
             media2 = vida_personaje2 + defensa_personaje2 + velocidad_personaje2 + ataque_personaje2;
         }
-        //fase de ataque
+        int vida_personaje_inicial = vida_personaje;
+        int vida_personaje2_inicial = vida_personaje2;
 
-        if (velocidad_personaje == velocidad_personaje2){
+        // Mostrar la vida inicial de cada jugador antes de la batalla
+        System.out.print("Jugador 1 [");
+        for (int i = 0; i < vida_personaje / 10; i++) System.out.print("❤️");
+        if (vida_personaje % 10 > 0) System.out.print("💔");
+        System.out.println("] " + vida_personaje);
+
+        System.out.print("Jugador 2 [");
+        for (int i = 0; i < vida_personaje2 / 10; i++) System.out.print("❤️");
+        if (vida_personaje2 % 10 > 0) System.out.print("💔");
+        System.out.println("] " + vida_personaje2);
+
+        //Si son igual de rapidos hacer que uno sea mas rapido de forma aleatoria
+
+        if (velocidad_personaje == velocidad_personaje2) {
             System.out.println("Tienen la misma valocidad asi que se va a decidir de forma aleatoria");
             Random random = new Random();
             int velocidad_aleatoria = 0;
             velocidad_aleatoria = random.nextInt(3) + 1;
-            if (velocidad_aleatoria ==1){
+            if (velocidad_aleatoria == 1) {
                 System.out.println("Ha salido el Personaje 1, asi que tiene 1 punto mas de velocidad");
                 velocidad_personaje++;
 
-            }else{
+            } else {
                 System.out.println("Ha salido el Personaje 2, asi que tiene 1 punto mas de velocidad");
                 velocidad_personaje2++;
             }
         }
+        System.out.println("\n¡Comienza la batalla!\n");
 
-        while (vida_personaje > 0 || vida_personaje2 > 0) {
-            System.out.println("Ronda"+ronda);
+        //bucle para el ataque hasta que 1 llegue a 0 de vida
+        while (vida_personaje > 0 && vida_personaje2 > 0) {
+            System.out.println("\n **********Ronda" + ronda + "********** \n");
+            //ataque jugador 1 (primero)
             if (velocidad_personaje > velocidad_personaje2) {
                 ataque1 = ataque_personaje - defensa_personaje2;
-                if (ataque1 <= 0) ataque1=10;
-                Random random = new Random();
-                vida_personaje2 = vida_personaje2 - random.nextInt(ataque1) + 1;
-                System.out.println("Personaje 1 ha quitado " + vida_personaje2 + " de vida");
-                System.out.println("La vida de Personaje 2 es " + vida_personaje2);
+                if (ataque1 <= 0) ataque1 = 10;
+                Random random1 = new Random();
+                vida_personaje2 = vida_personaje2 - random1.nextInt(ataque1) + 1;
                 if (vida_personaje2 < 0) vida_personaje2 = 0;
+                // Barra de vida simple Jugador 2
+                System.out.print("Jugador 2 [");
+                for (int i = 0; i < vida_personaje2 / 10; i++) System.out.print("❤️");
+                // medio corazón si queda entre 1 y 9
+                if (vida_personaje2 % 10 > 0) System.out.print("💔");
+                System.out.println("] " + vida_personaje2);
                 if (vida_personaje2 == 0) {
-                    System.out.println("El Jugador 2 ya no  tiene vida, el ganador es el Jugador 1");
+                    System.out.println("***El Jugador 2 ya no  tiene vida, el ganador es el Jugador 1***");
                 } else {
+                    //ataque jugador 2 (segudno)
                     ataque2 = ataque_personaje2 - defensa_personaje;
-                    if (ataque2 <= 0) ataque1=10;
-                    vida_personaje = vida_personaje - random.nextInt(ataque2) + 1;
-                    System.out.println("Personaje 2 ha dejado a " + vida_personaje + " de vida a Personaje 1");
-                    System.out.println("La vida de Personaje 1 es " + vida_personaje);
+                    if (ataque2 <= 0) ataque2 = 10;
+                    vida_personaje = vida_personaje - random1.nextInt(ataque2) + 1;
                     if (vida_personaje < 0) vida_personaje = 0;
+                    // Barra de vida simple Jugador 1
+                    System.out.print("Jugador 1 [");
+                    for (int i = 0; i < vida_personaje / 10; i++) System.out.print("❤️");
+                    // medio corazón si queda entre 1 y 9
+                    if (vida_personaje % 10 > 0) System.out.print("💔");
+                    System.out.println("] " + vida_personaje);
+
                     if (vida_personaje == 0) {
-                        System.out.println("El Jugador 1 ya no tiene vida, el ganador es el Jugador 2");
+                        System.out.println("***El Jugador 1 ya no tiene vida, el ganador es el Jugador 2***");
                     }
                 }
-            }else {
+            } else {
+                // ataque jugador 2 (primero)
                 ataque2 = ataque_personaje2 - defensa_personaje;
-                if (ataque2 <= 0) ataque1=10;
-                Random random = new Random();
-                vida_personaje = vida_personaje - random.nextInt(ataque2) + 1;
+                if (ataque2 <= 0) ataque2 = 10;
+                Random random2 = new Random();
+                vida_personaje = vida_personaje - random2.nextInt(ataque2) + 1;
                 if (vida_personaje < 0) vida_personaje = 0;
+                // Barra de vida simple Jugador 1
+                System.out.print("Jugador 1 [");
+                for (int i = 0; i < vida_personaje / 10; i++) System.out.print("❤️");
+                // medio corazón si queda entre 1 y 9
+                if (vida_personaje % 10 > 0) System.out.print("💔");
+                System.out.println("] " + vida_personaje);
                 if (vida_personaje == 0) {
-                    System.out.println("El Jugador 1 ya no tiene vida, el ganador es el Jugador 2");
-                }else{
+                    System.out.println("***El Jugador 1 ya no tiene vida, el ganador es el Jugador 2***");
+                } else {
+                    // ataquejugador 1 (segundo)
                     ataque1 = ataque_personaje - defensa_personaje2;
-                    if (ataque1 <= 0) ataque1=10;
-                    vida_personaje2 = vida_personaje2 - random.nextInt(ataque1) + 1;
+                    if (ataque1 <= 0) ataque1 = 10;
+                    vida_personaje2 = vida_personaje2 - random2.nextInt(ataque1) + 1;
                     if (vida_personaje2 < 0) vida_personaje2 = 0;
+                    // Barra de vida simple Jugador 2
+                    System.out.print("Jugador 2 [");
+                    for (int i = 0; i < vida_personaje2 / 10; i++) System.out.print("❤️");
+                    // medio corazón si queda entre 1 y 9
+                    if (vida_personaje2 % 10 > 0) System.out.print("💔");
+                    System.out.println("] " + vida_personaje2);
                     if (vida_personaje2 == 0) {
-                        System.out.println("El jugador 2 ya no tiene vida, el ganador es el Jugador 1");
+                        System.out.println("***El jugador 2 ya no tiene vida, el ganador es el Jugador 1***");
                     }
                 }
             }
