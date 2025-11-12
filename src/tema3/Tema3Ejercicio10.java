03aa8911a8235e52c1b601ca23234443292fc9bf
Daniel BT <danibt757@gmail.com>
2025-11-12 13:00:51 +0100

    Add files via upload

---
+++ src/tema3/Tema3Ejercicio10.java
@@ -0,0 +1,55 @@
+package tema3;
+
+import java.util.Scanner;
+
+public class Tema3Ejercicio10 {
+    public static void menu (char opcion, String[] args){
+        switch (opcion){
+            case 'a':
+                Tema3Ejercicio1.main(args);
+                break;
+            case 'b':
+                Tema3Ejercicio2.main(args);
+                break;
+            case 'c':
+                Tema3Ejercicio3.main(args);
+                break;
+            case 'd':
+                Tema3Ejercicio4.main(args);
+                break;
+            case 'e':
+                Tema3Ejercicio5.main(args);
+                break;
+            case 'f':
+                Tema3Ejercicio6.main(args);
+                break;
+            case 'g':
+                Tema3Ejercicio7.main(args);
+                break;
+            case 'h':
+                Tema3Ejercicio8.main(args);
+                break;
+            case 'i':
+                Tema3Ejercicio9.main(args);
+                break;
+        }
+    }
+    public static void menuEscrito(){
+        System.out.println("Que ejercicio quires realizar?");
+        System.out.println("a) Muestra el signo de un número introducido por el usuario.");
+        System.out.println("b) Indica si el usuario es mayor de edad o no.");
+        System.out.println("c) Calcula el área y perímetro de un círculo.");
+        System.out.println("d) Conversor de euros a dólares y de dólares a euros.");
+        System.out.println("e) Mostrar tabla de multiplicar de un número.");
+        System.out.println("f) Mostrar tablas de multiplicar del 1 al 10.");
+        System.out.println("g) Comprobador de números primos.");
+        System.out.println("h) Comprobador de fechas.");
+        System.out.println("i) Dibujar triángulos.");
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        menuEscrito();
+        char opcion = in.next().charAt(0);
+        menu(opcion,args);
+    }
+}
