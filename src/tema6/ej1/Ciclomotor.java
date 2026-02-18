0cf2e448375d4179044e8a5d34898c001617e86b
danbartor <danibt757@gmail.com>
2026-02-18 13:12:54 +0100

    .

--- src/tema6/ej1/Ciclomotor.java
+++ src/tema6/ej1/Ciclomotor.java
@@ -1,6 +1,32 @@
 package tema6.ej1;
 
-public class Ciclomotor {
+public class Ciclomotor extends Vehiculo{
     private int cilindrada;
 
+    //default
+    private int DEFAULT_CILINDRADA = 75;
+
+    public Ciclomotor (String matricula, String modelo, String marca,int cilindrada) {
+        super(matricula, modelo, marca);
+        this.cilindrada = cilindrada;
+    }
+
+    public Ciclomotor (){
+        this.cilindrada = DEFAULT_CILINDRADA;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+    }
+
+    public boolean carnet (int cilindrada){
+        boolean tiene = false;
+        if (cilindrada >= 125){
+            tiene = true;
+        }
+        return tiene;
+    }
 }
