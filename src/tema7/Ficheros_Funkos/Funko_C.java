372244914504bdfdcae80adb5e7bd5f7a94d2ba6
Daniel BT <danibt757@gmail.com>
2026-04-15 13:17:52 +0200

    Update Funko_C.java

--- src/tema7/Ficheros_Funkos/Funko_C.java
+++ src/tema7/Ficheros_Funkos/Funko_C.java
@@ -1,26 +1,38 @@
 package tema7.Ficheros_Funkos;
 
 public class Funko_C {
+    private String cod;
     private String nombre;
     private String modelo;
     private double precio;
-    private  int anio;
+    private  String anio;
 
+    private final String COD_DEFAULT = "123412";
     private final String NOMBRE_DEFAULT = "Spiderman";
     private final String MODELO_DEFAULT = "Marvel";
     private final double PRECIO_DEFAULT = 10.0;
-    private final int ANIO_DEFAULT = 2023;
-    public Funko_C(String nombre, String modelo, double precio, int anio) {
+    private final String ANIO_DEFAULT = "2023-01-01";
+    public Funko_C(String cod, String nombre, String modelo, double precio, String anio) {
+        this.cod = cod;
         this.nombre = nombre;
         this.modelo = modelo;
         this.precio = precio;
         this.anio = anio;
     }
     public Funko_C() {
+        this.cod = COD_DEFAULT;
         this.nombre =  NOMBRE_DEFAULT;
         this.modelo = MODELO_DEFAULT;
         this.precio = PRECIO_DEFAULT;
         this.anio = ANIO_DEFAULT;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
     }
 
     public String getNombre() {
@@ -47,15 +59,15 @@ public class Funko_C {
         this.precio = precio;
     }
 
-    public int getAnio() {
+    public String getAnio() {
         return anio;
     }
 
-    public void setAnio(int anio) {
+    public void setAnio(String anio) {
         this.anio = anio;
     }
 
     public String toString() {
-        return "Funko [Nombre=" + nombre + ", Modelo=" + modelo + ", Precio=" + precio + ", Año=" + anio + "]";
+        return "Funko [Cod=" + cod + ", Nombre=" + nombre + ", Modelo=" + modelo + ", Precio=" + precio + ", Año=" + anio + "]";
     }
 }
